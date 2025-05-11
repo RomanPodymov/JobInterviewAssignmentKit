013b0add5b522741ea215247fb4f4f1c9705a83a
Roman Podymov <podymfrombryansk@yandex.com>
2025-05-11 18:36:54 +0200

    BasicViewModel, ViewModel, BasicScreen, Screen

--- Source/JobInterviewAssignmentKit.swift
+++ Source/JobInterviewAssignmentKit.swift
@@ -1,11 +1,36 @@
-public protocol AnyScreen: AnyObject {
-    
+//
+//  JobInterviewAssignmentKit.swift
+//  JobInterviewAssignmentKit
+//
+//  Created by Roman Podymov on 11/05/2025.
+//  Copyright © 2025 Bookie. All rights reserved.
+//
+
+import UIKit
+
+public protocol Screen {
+    associatedtype ViewModelType
+
+    @MainActor
+    var viewModel: ViewModelType! { get }
 }
 
-open class AnyViewModel {
-    public unowned var screen: AnyScreen!
-    
-    public init(screen: AnyScreen!) {
+open class BasicScreen<VM: ViewModel>: UIViewController {
+    // swiftlint:disable:next force_cast
+    lazy var viewModel = VM(screen: self as! VM.ScreenType)
+}
+
+public protocol ViewModel {
+    associatedtype ScreenType
+
+    var screen: ScreenType! { get set }
+    init(screen: ScreenType)
+}
+
+open class BasicViewModel<ScreenType: Screen & AnyObject>: NSObject, ViewModel {
+    public unowned var screen: ScreenType!
+
+    public required init(screen: ScreenType) {
         self.screen = screen
     }
 }
